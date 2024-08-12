5fe0237decbd32e5da0c18bdfd66e1362c5bc34a
riddickalo <riddickxu333@gmail.com>
2024-08-12 18:17:32 +0800

    update run.sh, remove train.sh & train.py

--- run.sh
+++ run.sh
@@ -25,15 +25,6 @@ else
     exit 2
 fi
 
-
-# train.sh example
-# pipenv run python train.py --gpu_device 0 --dataset Sate1K_Thick --epochs 500  --notify_period 100 \
-# 				--model_size SkipDehamer --batch_size 48 --accum_grad_step 1 --lr 0.0005 --num_workers 8 \
-# 				--scheduler OneCycleLR --relu_type Leaky_ReLU --optimizer AdamW --bn_type GroupNorm --num_groups 3 \
-# 				--grad_clip_type clip_grad_norm --clip_grad_max_norm 1.0 --use_crop --use_bn --use_grad_clip \
-# 				--lambda_perceptual_loss 1.0 --lambda_l1_loss 10.0 --lambda_edge_loss 1.0 --lambda_ssim_loss 1.0 \
-# 				--use_l1_loss --use_edge_loss
-
 # test.sh example
 # pipenv run python test.py --gpu_device 0 --dataset 'Sate1K_Thick' --model_size SkipDehamer \
 #     --model_dir '2024-06-16_16' --model_name 'best_psnr.pth'
